58c0017e0d269da8cbfe92afde8638ee346ecd47
Parham Alvani <parham.alvani@gmail.com>
2020-04-04 18:39:08 +0430

    feat: Use Functions for Cleaner Code

--- main.c
+++ main.c
@@ -1,28 +1,83 @@
 #include <stdio.h>
 
-int ids[100];
-long long phones[100];
+#define CONTACTS_SIZE 100
+
+int ids[CONTACTS_SIZE];
+long long phones[CONTACTS_SIZE];
+
+// the first empty space in ids and phones array
+static int index;
+
+int menu(void) {
+    printf("1) Add\n");
+    printf("2) Get\n");
+    printf("3) Remove\n");
+    printf("4) List\n");
+    printf("5) Exit\n");
+    printf(">> ");
+
+    int choice;
+    scanf("%d", &choice);
+
+    return choice;
+}
+
+void contacts_print(void) {
+    int i;
+
+    for (i = 0; i < index; i++) {
+        printf("####\n");
+        printf("Index: %d\n", i);
+        printf("ID: %d\n", ids[i]);
+        printf("Phone: %lld\n", phones[i]);
+        printf("####\n");
+    }
+}
+
+long long contacts_get(int id) {
+    int i;
+
+    for (i = 0; i < index; i++) {
+        if (ids[i] == id) {
+            return phones[i];
+        }
+    }
+
+    return -1;
+}
+
+void contacts_remove(int id) {
+    int i;
+
+    for (i = 0; i < index; i++) {
+        if (ids[i] == id) {
+            ids[i] = ids[index - 1];
+            index--;
+            return;
+        }
+    }
+}
+
+void contacts_add(int id, long long phone) {
+    if (index == CONTACTS_SIZE) {
+        return;
+    }
+
+    ids[index] = id;
+    phones[index] = phone;
+    index++;
+}
 
 int main() {
     int is_run = 1;
-
-    // the first empty space in ids and phones array
-    int index = 0;
 
     int id;
     long long phone;
 
     while (is_run) {
-        printf("1) Add\n");
-        printf("2) Get\n");
-        printf("3) Remove\n");
-        printf("4) List\n");
-        printf("5) Exit\n");
-        printf(">> ");
-
         int i;
-        int choice;
-        scanf("%d", &choice);
+
+        int choice = menu();
 
         switch (choice) {
             case 1:
@@ -32,41 +87,28 @@ int main() {
                 printf("Phone: ");
                 scanf("%lld", &phone);
 
-                ids[index] = id;
-                phones[index] = phone;
-                index++;
-
+                contacts_add(id, phone);
                 break;
             case 2:
                 printf("ID: ");
                 scanf("%d", &id);
 
-                for (i = 0; i < index; i++) {
-                    if (ids[i] == id) {
-                        printf("Phone: 0%lld\n", phones[i]);
-                    }
+                phone = contacts_get(id);
+
+                if (phone == -1) {
+                    printf("ID not found");
+                } else {
+                    printf("Phone: 0%lld\n", phone);
                 }
                 break;
             case 3:
                 printf("ID: ");
                 scanf("%d", &id);
 
-                for (i = 0; i < index; i++) {
-                    if (ids[i] == id) {
-                        ids[i] = ids[index - 1];
-                        index--;
-                        continue;
-                    }
-                }
+                contacts_remove(id);
                 break;
             case 4:
-                for (i = 0; i < index; i++) {
-                    printf("####\n");
-                    printf("Index: %d\n", i);
-                    printf("ID: %d\n", ids[i]);
-                    printf("Phone: %lld\n", phones[i]);
-                    printf("####\n");
-                }
+                contacts_print();
                 break;
             case 5:
                 return 0;
