f9d73e925fd1ba6b6ab4d6c6c36d302620133c8e
Parham Alvani <parham.alvani@gmail.com>
2020-05-04 11:00:06 +0430

    feat: Add Example for Return from Pointers

---
+++ pointers/return.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+// do not return i's address
+int *return_pointer(int *p, int i) {
+  // static int s = 10; // you can return its address
+  // int a = 20; do not return its address
+
+  return p;
+}
+
+
+int main() {
+  int i = 78;
+
+  int *a = return_pointer(&i, i);
+
+  int j = 120;
+  j++;
+  printf("j: %d\n", j);
+
+  printf("a: %d\n", *a);
+}
